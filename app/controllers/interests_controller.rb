abdc4715a78d19a4e89462ba3047fa9516a86b8e
Ebenezer Williams <ebenezerw@me.com>
2017-02-25 22:42:46 -0500

    commit before playing with omniauth

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,2 +1,16 @@
 class InterestsController < ApplicationController
+
+  def create
+    @destination = Destination.find(params[:destination_id])
+    @interest = @destination.interests.create(interest_params)
+    redirect_to @destination
+
+  end
+private
+
+def interest_params
+  params.require(:interest).permit(:traveler_name, :social_link, :start_date, :end_date, :comment)
+
+end
+
 end
