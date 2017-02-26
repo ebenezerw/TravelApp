1dd9005c2091948a74078d7e442521f58e8a0340
Ebenezer Williams <ebenezerw@me.com>
2017-02-25 21:04:16 -0500

    full CRUD on Destination model complete

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,15 +1,41 @@
 class DestinationsController < ApplicationController
+    def index
+        @destination = Destination.all
+      end
 
-  def index
-      @destination = Destination.all
+    def show
+        @destination = Destination.find(params[:id])
     end
 
+    def new
+     end
 
+    def create
+        @destination = Destination.create(destination_params)
 
+        redirect_to @destination
+      end
 
+    def edit
+      @destination = Destination.find(params[:id])
+     end
 
+    def update
+      @destination = Destination.find(params[:id])
+      @destination.update(destination_params)
+      redirect_to @destination
+     end
 
+     def destroy
+       @destination = Destination.find(params[:id])
+       @destination.destroy
+       redirect_to destinations_path
 
+     end
 
+    private
 
+    def destination_params
+        params.require(:destination).permit(:place, :traveler_name, :social_link, :start_date, :end_date, :intro)
+      end
 end
