68b4fbfe163d6a974214be750ffee1e4220481a4
Ebenezer Williams <ebenezerw@me.com>
2017-04-12 18:40:03 -0400

    migrate db

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,11 +1,9 @@
 class Destination < ApplicationRecord
+    default_scope { order('updated_at DESC') }
+    validates :place, presence: true, length: { minimum: 2 }
+    validates :start_date, presence: true
+    validates :end_date, presence: true
 
-  default_scope { order('updated_at DESC') }
-  validates :place,  presence: true, length: {minimum: 2}
-  validates :start_date,  presence: true
-  validates :end_date,  presence: true
-
-
-  has_many :interests, dependent: :destroy
-  belongs_to :user
+    has_many :interests, dependent: :destroy
+    belongs_to :user
 end
