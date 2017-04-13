c24a85f8d477338db5f42fdc3be59f33dd512876
Ebenezer Williams <ebenezerw@me.com>
2017-04-12 20:58:21 -0400

    removed message index page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,14 +1,14 @@
 class ConversationsController < ApplicationController
 
-    def index
-        @users = User.all
-        @conversations = Conversation.all
-     end
+    # def index
+    #     @users = User.all
+    #     @conversations = Conversation.all
+    #  end
 
 
 
     def create
-    
+
         if Conversation.between(params[:sender_id], params[:recipient_id])
                        .present?
             @conversation = Conversation.between(params[:sender_id],
