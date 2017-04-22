c3f94e2455babfc99a3343b7e592182168a2160f
Ebenezer Williams <ebenezerw@me.com>
2017-04-22 19:39:20 -0400

    facebook env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '582887531905771', 'ddbf0ea0e44019bb86ffc313db783d57', :image_size => 'large',
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], :image_size => 'large',
     scope: 'public_profile', info_fields: 'id,name,link', dispaly: 'popup', secure_image_url: true
 end
 
